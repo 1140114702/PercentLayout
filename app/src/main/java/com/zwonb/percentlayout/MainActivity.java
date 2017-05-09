5a3ba4b558a9a40680f65515f01a2e04f7d6f544
bin <1140114702@qq.com>
2017-05-09 23:06:49 +0800

    itemview是正方形时的比例

--- app/src/main/java/com/zwonb/percentlayout/MainActivity.java
+++ app/src/main/java/com/zwonb/percentlayout/MainActivity.java
@@ -1,13 +1,57 @@
 package com.zwonb.percentlayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewTreeObserver.OnGlobalLayoutListener {
+
+    private TextView top_view;
+    private TextView left_view;
+    private TextView right_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        top_view = (TextView) findViewById(R.id.top_view);
+        left_view = (TextView) findViewById(R.id.left_view);
+        right_view = (TextView) findViewById(R.id.right_view);
+
+        top_view.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        left_view.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        right_view.getViewTreeObserver().addOnGlobalLayoutListener(this);
+    }
+
+    @Override
+    public void onGlobalLayout() {
+        top_view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        top_view.setText("aspectRatio=298%,width=" + top_view.getWidth()
+                + ",height=" + top_view.getHeight());
+
+        left_view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        LinearLayout.LayoutParams leftParams = (LinearLayout.LayoutParams) left_view.getLayoutParams();
+        left_view.setText("aspectRatio=100%,width=" + left_view.getWidth()
+                + ",height=" + left_view.getHeight()
+                + ",ml=" + leftParams.leftMargin
+                + ",mt=" + leftParams.topMargin
+                + ",mr=" + leftParams.rightMargin
+                + ",mb=" + leftParams.bottomMargin);
+
+        right_view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        LinearLayout.LayoutParams rightParams = (LinearLayout.LayoutParams) left_view.getLayoutParams();
+        right_view.setText("aspectRatio=100%,width=" + right_view.getWidth()
+                + ",height=" + right_view.getHeight()
+                + ",ml=" + rightParams.leftMargin
+                + ",mt=" + rightParams.topMargin
+                + ",mr=" + rightParams.rightMargin
+                + ",mb=" + rightParams.bottomMargin);
+
     }
 }
--- app/src/main/java/com/zwonb/percentlayout/PercentLinearLayout.java
+++ app/src/main/java/com/zwonb/percentlayout/PercentLinearLayout.java
@@ -8,39 +8,47 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 /**
- *
  * The attributes that you can use are:
  * <ul>
- *     <li>{@code layout_widthPercent}
- *     <li>{@code layout_heightPercent}
- *     <li>{@code layout_marginPercent}
- *     <li>{@code layout_marginLeftPercent}
- *     <li>{@code layout_marginTopPercent}
- *     <li>{@code layout_marginRightPercent}
- *     <li>{@code layout_marginBottomPercent}
- *     <li>{@code layout_marginStartPercent}
- *     <li>{@code layout_marginEndPercent}
- *     <li>{@code layout_aspectRatio} 根据长比高的比例自动计算尺寸
+ * <li>{@code layout_widthPercent}
+ * <li>{@code layout_heightPercent}
+ * <li>{@code layout_marginPercent}
+ * <li>{@code layout_marginLeftPercent}
+ * <li>{@code layout_marginTopPercent}
+ * <li>{@code layout_marginRightPercent}
+ * <li>{@code layout_marginBottomPercent}
+ * <li>{@code layout_marginStartPercent}
+ * <li>{@code layout_marginEndPercent}
+ * <li>{@code layout_aspectRatio} 根据长比高的比例自动计算尺寸
  * </ul>
- *
+ * <p>
  * Created by zyb on 2017/5/9.
  */
 
 public class PercentLinearLayout extends LinearLayout {
 
-    private PercentLayoutHelper mPercentLayoutHelper;
+    private PercentLayoutHelper mHelper;
 
     public PercentLinearLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mPercentLayoutHelper = new PercentLayoutHelper(this);
+        mHelper = new PercentLayoutHelper(this);
     }
 
+//    @Override
+//    protected LayoutParams generateDefaultLayoutParams() {
+//        return new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+//    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        mPercentLayoutHelper.adjustChildren(widthMeasureSpec, heightMeasureSpec);
+        mHelper.adjustChildren(widthMeasureSpec, heightMeasureSpec);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        if (mPercentLayoutHelper.handleMeasuredStateTooSmall()) {
+        if (mHelper.handleMeasuredStateTooSmall()) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
     }
@@ -48,14 +56,8 @@ public class PercentLinearLayout extends LinearLayout {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        mPercentLayoutHelper.restoreOriginalParams();
+        mHelper.restoreOriginalParams();
     }
-
-    @Override
-    public LayoutParams generateLayoutParams(AttributeSet attrs) {
-        return new LayoutParams(getContext(), attrs);
-    }
-
 
     public static class LayoutParams extends LinearLayout.LayoutParams
             implements PercentLayoutHelper.PercentLayoutParams {
@@ -66,20 +68,9 @@ public class PercentLinearLayout extends LinearLayout {
             mPercentLayoutInfo = PercentLayoutHelper.getPercentLayoutInfo(c, attrs);
         }
 
-        @Override
-        public PercentLayoutHelper.PercentLayoutInfo getPercentLayoutInfo() {
-            return mPercentLayoutInfo;
-        }
-
-        @Override
-        protected void setBaseAttributes(TypedArray a, int widthAttr, int heightAttr) {
-            PercentLayoutHelper.fetchWidthAndHeight(this, a, widthAttr, heightAttr);
-        }
-
         public LayoutParams(int width, int height) {
             super(width, height);
         }
-
 
         public LayoutParams(ViewGroup.LayoutParams source) {
             super(source);
@@ -87,6 +78,24 @@ public class PercentLinearLayout extends LinearLayout {
 
         public LayoutParams(MarginLayoutParams source) {
             super(source);
+        }
+
+//        public LayoutParams(LinearLayout.LayoutParams source) {
+//            super((MarginLayoutParams) source);
+//            gravity = source.gravity;
+//        }
+
+        @Override
+        public PercentLayoutHelper.PercentLayoutInfo getPercentLayoutInfo() {
+            if (mPercentLayoutInfo == null) {
+                mPercentLayoutInfo = new PercentLayoutHelper.PercentLayoutInfo();
+            }
+            return mPercentLayoutInfo;
+        }
+
+        @Override
+        protected void setBaseAttributes(TypedArray a, int widthAttr, int heightAttr) {
+            PercentLayoutHelper.fetchWidthAndHeight(this, a, widthAttr, heightAttr);
         }
 
     }
